a922d28a7f2a276eb310daa776d923403edf585f
A.Parsa <develop.apm@gmail.com>
2021-12-05 08:37:33 +0330

    add Person struct and people array

--- simple-storage/SimpleStorage.sol
+++ simple-storage/SimpleStorage.sol
@@ -3,11 +3,22 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     uint256 favoriteNumber;
 
+    struct Person {
+        string name;
+        uint256 number;
+    }
+
+    Person[] public people;
+
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
 
     function retrieve() public view returns (uint256) {
         return favoriteNumber;
+    }
+
+    function addPerson(string memory name, uint256 number) public {
+        people.push(Person({name: name, number: number}));
     }
 }
