9b9c4ad06dc5bb508f21e8c34737eec93f6c144f
A.Parsa <develop.apm@gmail.com>
2021-12-05 08:42:22 +0330

    add map name to fav number

--- simple-storage/SimpleStorage.sol
+++ simple-storage/SimpleStorage.sol
@@ -9,6 +9,7 @@ contract SimpleStorage {
     }
 
     Person[] public people;
+    mapping(string => uint256) public nameToFavNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
@@ -20,5 +21,6 @@ contract SimpleStorage {
 
     function addPerson(string memory name, uint256 number) public {
         people.push(Person({name: name, number: number}));
+        nameToFavNumber[name] = number;
     }
 }
